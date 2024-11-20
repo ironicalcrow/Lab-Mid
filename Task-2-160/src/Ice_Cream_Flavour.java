078896f8ecd2fba208632c6f34c9f8afcdce3399
arham <arhamkhan@iut-dhaka.edu>
2024-11-20 15:27:07 +0600

    first class I have thought of

---
+++ Task-2-160/src/Ice_Cream_Flavour.java
@@ -0,0 +1,29 @@
+public class Ice_Cream_Flavour {
+    private String name;
+    private double price;
+    private int quantity;
+    public Ice_Cream_Flavour(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
+
