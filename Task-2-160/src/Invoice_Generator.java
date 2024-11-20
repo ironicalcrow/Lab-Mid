584277bacb5643a91cd253bd10449c7dc3cd27f6
arham <arhamkhan@iut-dhaka.edu>
2024-11-20 16:23:50 +0600

    Inheritance instead of interface

--- Task-2-160/src/Invoice_Generator.java
+++ Task-2-160/src/Invoice_Generator.java
@@ -1,12 +1,38 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Invoice_Generator {
     private Invoice_Generator instance;
-    public Invoice_Generator getInstance()
-    {
-        if(instance==null)
-        {instance=new Invoice_Generator();}
+    private String filename;
+    public Invoice_Generator getInstance() {
+        if (instance == null) {
+            instance = new Invoice_Generator();
+        }
         return instance;
     }
 
-    public void invoice(String S){}
+    public void invoice(Order o) {
+        filename= "Order"+ o.getOrderID();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename, true))) {
+            String s = "Ice Cream Shop Invoice: ";
+            bw.write(s);
+            bw.append("Order Number is--" + o.getOrderID());
+            int icf = o.orderICFsize();
+            int ict = o.orderICTsize();
+            for (int i = 0; i < icf; i++) {
+                bw.append(o.getF(i).getName() + " - " + o.getF(i).getQuantity() + " scoop:" + o.getF(i).initialCalc());
+            }
+            for (int i = 0; i < ict; i++) {
+                bw.append(o.getT(i).getName() + " - " + o.getT(i).getQuantity() + " time:" + o.getT(i).initialCalc());
+            }
+            bw.append("Subtotal: " + o.price());
+            bw.append("Tax: " + o.getTax());
+            bw.append("Total: " + o.SubTotal());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+    }
 }
--- Task-2-160/src/Order.java
+++ Task-2-160/src/Order.java
@@ -1,20 +1,34 @@
 import java.util.ArrayList;
 
 public class Order {
-    private int orderID;
+    private int orderID=0;
     private double tax= 0.8;
     private ArrayList<Ice_Cream_Flavour> f;
     private ArrayList<Ice_Cream_Toppings> t;
     public Order() {
+        orderID++;
         f = new ArrayList();
         t = new ArrayList();
     }
+
     public void addIceCreamFlavour(Ice_Cream_Flavour order) {
         this.f.add(order);
     }
     public void addIceCreamToppings(Ice_Cream_Toppings order) {
         this.t.add(order);
     }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public Ice_Cream_Flavour getT(int index) {
+        return f.get(index);
+    }
+    public Ice_Cream_Toppings getF(int index) {
+        return t.get(index);
+    }
+
     public double price()
     {
         double sum=0;
@@ -22,11 +36,28 @@ public class Order {
         {
             sum= i.initialCalc();
         }
-        for(Ice_Cream_Toppings t : t)
+        for(Ice_Cream_Toppings i : t)
         {
-            sum= t.initialCalc();
+            sum= i.initialCalc();
         }
-        return sum*tax;
+        return sum;
+    }
+    public double SubTotal()
+    {
+        double sum= price();
+        return sum*tax+sum;
+    }
+    public int orderICFsize()
+    {
+        return f.size();
+    }
+    public int orderICTsize()
+    {
+        return t.size();
+    }
+    public double getTax()
+    {
+        return tax*price();
     }
 
 
